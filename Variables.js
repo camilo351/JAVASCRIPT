f435b5c2a53b8a52ce2a734c2aa2a7380ff540f9
camilo351 <camilo.hernandez.0627@gmail.com>
2025-04-02 23:32:44 -0500

    EJERCICIOS CON VARIABLES

---
+++ Variables.js
@@ -0,0 +1,103 @@
+// EJERCICIO 1
+// let nombre = "Camilo";
+// console.log(nombre);
+
+// EJERCICIO 2
+// let edad = 18;
+// let año = 2025;
+// let añosEdad = 18 + 5;
+// console.log(añosEdad);
+
+// EJERCICIO 3
+// let precioProducto = 2500;
+// let cantidadProducto = 2;
+// let totalPagar = precioProducto * cantidadProducto;
+// console.log(totalPagar);
+
+// EJERCICIO 4
+// let gradosCelcius = 25;
+// let ConversionFahrenheit = (gradosCelcius * 9/5) + 32
+// console.log(ConversionFahrenheit);
+
+// EJERCICIO 5
+// let nombre = 'Camilo';
+// let apellido = 'Hernandez';
+// console.log(nombre + ' ' + apellido);
+
+// EJERCICIO 6
+// let num1 = 30;
+// let num2 = 34;
+// let num3 = 23;
+// promedio = (num1 + num2 + num3) / 3;
+// console.log(promedio);
+
+// EJERCIIO 7
+// let producto = 1423000;
+// let IVA = 0.19;
+// let resultado = producto * IVA;
+// console.log(resultado);
+
+// EJERCICIO 8
+// let añoDia = 365;
+// let horaDia = 24;
+// let horaAño = añoDia * horaDia;
+// console.log(horaAño);
+
+// EJERCICIO 9
+// let numero = 8
+// console.log (numero * 2);
+
+// EJERCICIO 10
+// let frase = "Hola Mundo";
+// console.log(frase.length);
+
+// EJERCICIO 11
+// let numero = 12.80
+// console.log(Math.round(numero));
+
+// EJERCICIO 12
+// let num1 = 29
+// let num2 = 299
+// console.log(Math.max(num1, num2));
+
+// EJERCICIO 13
+// let numero = 15;
+// if (numero % 2 == 0) {
+//     console.log (numero,"es par");
+// } else {
+//     console.log (numero, "es impar");
+// };
+
+// EJERCICIO 14
+// let frase = "hola mundo";
+// console.log(frase.toUpperCase());
+
+// EJERCICIO 15
+// let dolares = 100;
+// let conversionEuros = dolares * 0.92;
+// console.log(Math.round(conversionEuros));
+
+// EJERCICIO 16
+// let añoNacimiento = 2007;
+// let añoActual = 2025;
+// console.log((añoActual - añoNacimiento)* 365);
+
+// EJERCICIO 17
+// let frase = "HOLA MUNDO";
+// let resultado = frase.replace(/\s+/g, "");
+// console.log(resultado);
+
+// EJERCICIO 18
+// let verdadero = true;
+// let falso = false;
+// console.log (verdadero || false); //true
+// console.log (verdadero && false), // false
+// console.log (!verdadero); // false
+
+// EJERCICIO 19
+// let numero = 10;
+// console.log(Math.sqrt(numero));
+
+// EJERCICIO 20
+// let numero = 666
+// console.log(String(numero));
